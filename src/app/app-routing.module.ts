fae2e5c2d5609e9fe507156feb54b1de1350501e
polland <pollandsabisa@gmail.com>
2023-04-21 16:11:52 +0300

    responsive tables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TablesComponent } from './tables/tables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:TablesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
